33efa2b18f9b473946155962487d4cf1857d8fba
LeonardoAugusto <leonardo.sym2304@gmail.com>
2020-03-03 22:19:31 -0300

    Alteração na Descrição da Aplicação

--- SistemaRecados/Controllers/HomeController.cs
+++ SistemaRecados/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace SistemaRecados.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Esse é o FUCKING Sistema de Recados do LEONARDO, Oh Yeah!";
 
             return View();
         }
